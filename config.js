5a7084c8c59c77f810204617d93db4d77669e0d5
taechit.cha <taechit.c@gmail.com>
2021-05-31 20:43:57 +0700

    init firebase config file

---
+++ config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "API_KEY_HERE",
+    authDomain: "AUTH_DOMAIN_HERE",
+    databaseURL: "DATABASE_URL_HERE",
+    projectId: "PROJECT_ID_HERE",
+    storageBucket: "STORAGE_BUCKET_HERE",
+    messagingSenderId: "MESSAGING_ID_HERE"
+  }// Exports
+  
+  module.exports = firebaseConfig;
