ae50689574b4a29159223ab915f496cf2bed65ef
taechit.cha <taechit.c@gmail.com>
2021-05-31 20:59:29 +0700

    update config

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const jsonToFirestore = async () => {
     await firestoreService.initializeApp(serviceAccount, firebaseConfig.databaseURL);
     console.log('Firebase Initialized');
 
-    await firestoreService.restore('./asset/thailand.json');
+    await firestoreService.restore('./assets/thailand.json');
     console.log('Upload Success');
   }
   catch (error) {
